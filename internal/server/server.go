aee18758bee7fea4e0a8621adb071719bbb78982
Ashish Kumar <143941235+ashish0kumar@users.noreply.github.com>
2025-12-22 23:58:13 +0530

    wip

---
+++ internal/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"tcp2http/internal/response"
+)
+
+type Server struct {
+	closed bool
+}
+
+func runConnection(_ *Server, conn io.ReadWriteCloser) {
+	defer conn.Close()
+
+	headers := response.GetDefaultHeaders(0)
+	response.WriteStatusLine(conn, response.StatusOK)
+	response.WriteHeaders(conn, headers)
+}
+
+func runServer(s *Server, listener net.Listener) {
+	for {
+		conn, err := listener.Accept()
+		if s.closed {
+			return
+		}
+		if err != nil {
+			return
+		}
+		go runConnection(s, conn)
+	}
+}
+
+func Serve(port uint16) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	server := &Server{closed: false}
+	go runServer(server, listener)
+
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed = true
+	return nil
+}
