5fc9bf9ec57a43cfd653fff91b3e86de94039067
Ashish Kumar <143941235+ashish0kumar@users.noreply.github.com>
2025-12-26 20:15:02 +0530

    http responses done

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,17 +1,76 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"tcp2http/internal/request"
+	"tcp2http/internal/response"
 	"tcp2http/internal/server"
 )
 
 const port = 42069
 
+func respond400() []byte {
+	return []byte(`<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>`)
+}
+
+func respon500() []byte {
+	return []byte(`<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`)
+}
+
+func respond200() []byte {
+	return []byte(`<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`)
+}
+
 func main() {
-	s, err := server.Serve(port)
+	s, err := server.Serve(port, func(w *response.Writer, req *request.Request) {
+		h := response.GetDefaultHeaders(0)
+		body := respond200()
+		status := response.StatusOK
+
+		switch req.RequestLine.RequestTarget {
+		case "/yourproblem":
+			body = respond400()
+			status = response.StatusBadRequest
+		case "/myproblem":
+			body = respon500()
+			status = response.StatusInternalServerError
+		}
+
+		h.Replace("Content-length", fmt.Sprintf("%d", len(body)))
+		h.Replace("Content-type", "text/html")
+		w.WriteStatusLine(status)
+		w.WriteHeaders(*h)
+		w.WriteBody(body)
+	})
+
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
